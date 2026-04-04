318e907c0cb7d1e961d54a027971ce2c8e9def6d
Lluís Merlos Pieri <llmerlos@outlook.com>
2026-04-04 19:58:34 +0200

    wip

--- app/src/main.c
+++ app/src/main.c
@@ -1,87 +1,24 @@
 /*
  * Copyright (c) 2021 Nordic Semiconductor ASA
- * SPDX-License-Identifier: Apache-2.0
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
-
-#include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
-#include <zephyr/logging/log.h>
-
-#include <app/drivers/blink.h>
-
-#include <app_version.h>
-
 #include <app_event_manager.h>
 
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+#define MODULE main
+#include <caf/events/module_state_event.h>
 
-#define BLINK_PERIOD_MS_STEP 100U
-#define BLINK_PERIOD_MS_MAX  1000U
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(MODULE);
+
 
 int main(void)
 {
-	int ret;
-	unsigned int period_ms = BLINK_PERIOD_MS_MAX;
-	const struct device *sensor, *blink;
-	struct sensor_value last_val = {0}, val;
-
-	LOG_INF("Zephyr Example Application %s", APP_VERSION_STRING);
-
-	ret = app_event_manager_init();
-	if (ret < 0) {
-		LOG_ERR("Could not initialize event manager (%d)", ret);
-		return 0;
-	}
-
-	// random shit
-
-	sensor = DEVICE_DT_GET(DT_NODELABEL(example_sensor));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return 0;
-	}
-
-	blink = DEVICE_DT_GET(DT_NODELABEL(blink_led));
-	if (!device_is_ready(blink)) {
-		LOG_ERR("Blink LED not ready");
-		return 0;
-	}
-
-	ret = blink_off(blink);
-	if (ret < 0) {
-		LOG_ERR("Could not turn off LED (%d)", ret);
-		return 0;
-	}
-
-	LOG_INF("Use the sensor to change LED blinking period");
-
-	while (1) {
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return 0;
-		}
-
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return 0;
-		}
-
-		if ((last_val.val1 == 0) && (val.val1 == 1)) {
-			if (period_ms == 0U) {
-				period_ms = BLINK_PERIOD_MS_MAX;
-			} else {
-				period_ms -= BLINK_PERIOD_MS_STEP;
-			}
-
-			LOG_INF("Proximity detected, setting LED period to %u ms", period_ms);
-			blink_set_period_ms(blink, period_ms);
-		}
-
-		last_val = val;
-
-		k_sleep(K_MSEC(100));
+	if (app_event_manager_init()) {
+		LOG_ERR("Application Event Manager not initialized");
+	} else {
+		LOG_INF("Application Event Manager Initialized");
+		module_set_state(MODULE_STATE_READY);
 	}
 
 	return 0;
